5712f62c55b1ab4de8faf5aefcac823961abd3c1
Damodardev1 <dhamuvanjarapu2002@gmail.com>
2025-06-08 01:47:44 +0530

    ssl

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+              'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', base_path('storage/certs/ca.pem')),
             ]) : [],
         ],
